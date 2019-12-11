48dc84bf2456fa8ea3dd5c90ad3d28a231d536ab
sicfreeze <40338658+sicfreeze@users.noreply.github.com>
2019-12-11 08:12:11 +0700

    2

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-//ทำการ import express เข้ามาใช้งาน โดยสร้างตัวแปร express ขึ้นมาเพื่อรับค่า
-const express = require('express')
-//ทำการสร้าง Instance ของ express และสร้างตัวแปร app ขึ้นมาเพื่อรับค่า
-const app = express()
-//สร้างตัวแปร PORT ขึ้นมารับค่า port ในกรณีที่เราได้กำหนดไว้ใน environment ของเครื่อง
-//แต่ถ้าไม่ได้กำหนดไว้ เราจะใช้ค่า 8080 แทน
-const PORT = process.env.PORT || 8080
-//สร้าง route ขึ้นมา 1 ตัว โดยกำหนดให้ path คือ / หรือ index ของ host นั่นเอง
-//จากนั้นให้กำหนด response แสดงคำว่า Hello World
-app.get('/', (req, res) => res.send('Hello World'))
-//run web server ที่เราสร้างไว้ โดยใช้ PORT ที่เรากำหนดไว้ในตัวแปร PORT
-app.listen(PORT, () => {
-    //หากทำการ run server สำเร็จ ให้แสดงข้อความนี้ใน cmd หรือ terminal
-    console.log(`Server is running on port : ${PORT}`)
-})
-//ทำการ export app ที่เราสร้างขึ้น เพื่อให้สามารถนำไปใช้งานใน project อื่นๆ 
-//เปรียบเสมือนเป็น module ตัวนึง
-module.exports = app
+var express = require("express");
+var app = express();
+
+var port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    var responseObject = {
+        status: true,
+        data: { text: "hello" }
+    }
+    res.json(responseObject);
+});
+
+app.listen(port, () => {
+   console.log("application is listening on:", port);
+});
