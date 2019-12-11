c83b40ffcf9947309efdbbc999a8c1b5cf88b157
sicfreeze <40338658+sicfreeze@users.noreply.github.com>
2019-12-12 00:59:21 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -20,18 +20,18 @@ restService.post("/cate", function(req, res) {
   var speech = '';
   if((req.body.queryResult &&
     req.body.queryResult.parameters &&
-    req.body.queryResult.parameters.echoText )
-    == ('อุณหภูมิภายในบ้าน' || ('อุณหภูมิ')) ){
+    req.body.queryResult.parameters.cate )
+    == ('อุณหภูมิภายในบ้าน' && ('อุณหภูมิ')) ){
       speech = 'รอสักครู่กำลังดึงข้อมูล อุณหภูมิภายในบ้าน'
     }else if((req.body.queryResult &&
       req.body.queryResult.parameters &&
-      req.body.queryResult.parameters.echoText )
+      req.body.queryResult.parameters.cate )
       == 'กล้องวงจรปิด' ){
        speech = 'รอสักครู่กำลังดึงข้อมูล กล้องวงจรปิด'
       }else if((req.body.queryResult &&
         req.body.queryResult.parameters &&
-        req.body.queryResult.parameters.echoText )
-        == ('ไฟฟ้า' || ('ไฟฟ้าภายในบ้าน')) ){
+        req.body.queryResult.parameters.cate )
+        == ('ไฟฟ้า' && ('ไฟฟ้าภายในบ้าน')) ){
           speech = 'รอสักครู่กำลังดึงข้อมูล ไฟฟ้าภายในบ้าน'
       }else
 
